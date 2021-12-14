476aa46f029d476562ab8dec79d5e12aa039ff41
Basil Sajid Shaikh <shaikh.basil786@gmail.com>
2021-12-14 01:51:13 -0800

    Committing Assignment 13 + merging A1 and 2

---
+++ Assignment 13/public/server.js
@@ -0,0 +1,71 @@
+var mongoose = require('mongoose')
+var dbURL = 'mongodb+srv://shubham:sd12345@cluster0-kblzo.mongodb.net/test?retryWrites=true&w=majority';
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var cors = require('cors')
+
+app.use(cors())
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+//Connection to MongoDB
+mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+    console.log("MongoDB connection : ", err)
+})
+
+//MongoDB Object
+var Product = mongoose.model('product', {
+    "id": Number,
+    "product": {
+        "productid": Number,
+        "category": String,
+        "price": Number,
+        "name": String,
+        "instock": Boolean
+    }
+});
+
+//Get Products API
+app.get('/product/get/', (req, res) => {
+    Product.find({}, (err, products) => {
+        let productsToDisplay = {};
+        products.forEach((prod) => {            
+            productsToDisplay[prod.id] = prod;
+        })
+        res.send(productsToDisplay)
+    })
+})
+
+//Create Product API
+app.post('/product/create', (req, res) => {
+    var product = new Product(req.body)
+    product.save((err) => {
+        if(err) {
+            res.sendStatus(500)
+        } else {
+            res.sendStatus(200)
+        }
+    })
+})
+
+//Update Product API
+app.post('/product/update/:id', (req, res) => {
+    Product.updateOne(req.params, req.body, (err, data) => {
+        res.redirect('/product/get');        
+    })
+})
+
+//Delete Product API
+app.get('/product/delete/:id', (req, res) => {
+    Product.deleteOne(req.params, (err, data) => {
+        res.redirect('/product/get');        
+    })  
+})
+
+//Server is running on port 3001 and client is running on port 3000
+var server = app.listen(3001, () => {
+    console.log('Server is listening on port : ', server.address().port)
+})
+
